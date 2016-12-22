dcc0a2817b4af6bd4cd9ddf55ecc51721511f9cd
madhavmantri <madman260895@gmail.com>
2016-12-22 19:43:50 +0530

    weight function data added

--- src/igraph_wpr.py
+++ src/igraph_wpr.py
@@ -8,8 +8,16 @@ teams_id = {"Afghanistan":0, "Australia":1,"Bangladesh":2,"England":3,"India":4,
 "Ireland":5, "New Zealand":6,"Pakistan":7,"South Africa":8,"Sri Lanka":9,
 "West Indies":10,"Zimbabwe":11,
 }
+
 linkmatrix = [[] for i in range(len(teams_id))]
 winmatrix = [[0 for j in range(len(teams_id))] for i in range(len(teams_id))]
+winrmatrix = [[0 for j in range(len(teams_id))] for i in range(len(teams_id))]
+winwmatrix = [[0 for j in range(len(teams_id))] for i in range(len(teams_id))]
+movrmatrix = [[0 for j in range(len(teams_id))] for i in range(len(teams_id))]
+movwmatrix = [[0 for j in range(len(teams_id))] for i in range(len(teams_id))]
+runmatrix = [[0 for j in range(len(teams_id))] for i in range(len(teams_id))]
+weight_matrix = [[0 for j in range(len(teams_id))] for i in range(len(teams_id))]
+
 played = [[0 for j in range(len(teams_id))] for i in range(len(teams_id))]
 df_train = pd.read_csv("../data/cricket.csv")
 
@@ -26,29 +34,41 @@ for i in df_train.index:
     played[loser_id][winner_id] += 1
     played[winner_id][loser_id] += 1
     winmatrix[loser_id][winner_id] += 1
+    if "runs" in df_train.Winnerby[i]:
+        winrmatrix[loser_id][winner_id] += 1
+        movrmatrix[loser_id][winner_id] += df_train.Margin[i]
+    elif "wickets" in df_train.Winnerby[i]:
+        winwmatrix[loser_id][winner_id] += 1
+        movrmatrix[loser_id][winner_id] += df_train.Margin[i]
+    toss_winner_id = teams_id[df_train.Toss_Winner[i]]
+    toss_loser_id = teams_id[df_train.Team1[i]]
+    if df_train.Team1[i] in df_train.Toss_Winner[i]:
+        toss_loser_id = teams_id[df_train.Team2[i]]
+    runmatrix[toss_loser_id][toss_winner_id] += df_train.Run1[i]
+    runmatrix[toss_winner_id][toss_loser_id] += df_train.Run2[i]
 
-print winmatrix
+# print winmatrix
 
 g = ig.Graph.Full(n = len(teams_id), directed = True)
 g = g.Adjacency(winmatrix, mode = "DIRECTED")
 g.vs["name"] = teams_id.keys()
 
 layout = g.layout("fruchterman_reingold")
-igraph.plot(g, '2.png', layout=layout, bbox=(1000, 1000), margin=120, hovermode='closest', labels = "name")
+# igraph.plot(g, '2.png', layout=layout, bbox=(1000, 1000), margin=120, hovermode='closest', labels = "name")
 
 # layout = g.layout("kk")
 # g.write_svg("newgraph1.svg", labels = "name" , layout=layout)
 # print g.ecount()
 
-print g
+# print g
 
 for i in range(len(teams_id)):
     for j in range(len(teams_id)):
         if played[i][j]!=0:
-            winmatrix[i][j] /= 1.*(played[i][j])
+            weight_matrix[i][j] = winmatrix[i][j] / (1.*(played[i][j]))
 
-print winmatrix
+print weight_matrix
 
-pr = g.pagerank(vertices = None, directed = True, damping = 0.85, weights = winmatrix, implementation="power")
+pr = g.pagerank(vertices = None, directed = True, damping = 0.85, weights = weight_matrix, implementation="power")
 print pr
 exit()
