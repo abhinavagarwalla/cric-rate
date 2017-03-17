1c4cee731c8b2c1f145a7a28a1a6d53461e001c0
Abhinav Agarwalla <navabhi174@gmail.com>
2017-03-18 01:06:18 +0530

    Starter code for HITS

--- src/player_hits.py
+++ src/player_hits.py
@@ -1,49 +1,23 @@
-import yaml
-import os, sys
-import csv
-import numpy as np
-from collections import defaultdict
 import pickle
+import numpy as np
+import igraph as ig
+import igraph.drawing
+import networkx as nx
+import matplotlib.pyplot as plt
 
-folderloc = "../data/odis/"
+fp = open('../data/playerdict.pkl')
+plist = pickle.load(fp)
+bats = np.unique([i[0] for i in plist.keys()])
+bowls = np.unique([i[1] for i in plist.keys()])
 
-data = list()
-headers = ["Date", "Team1", "Team2", "Toss_Winner", "Toss_Decision", "Venue", "Run1", "Over1", "Wicket1", "Run2", "Over2", "Wicket2", "Winner", "Winnerby", "Margin"]
-data.append(headers)
+# playermat = [[0 for i in range(len(bowls)+len(bats))] for j in range(len(bowls)+len(bats))]
+playermat = np.zeros((len(bowls)+len(bats), len(bowls)+len(bats)))
+for key, value in plist.iteritems():
+	playermat[np.where(bowls==key[1])[0][0]][len(bowls)+np.where(bats==key[0])[0][0]] = value
 
-playerlist = defaultdict(dict)
-for file in os.listdir(folderloc):
-	print file
-	if 'yaml' not in file:
-		continue
-	match = list()
-	filepath = folderloc + file
-	with open(filepath, 'r') as stream:
-	    try:
-	        file1 = yaml.load(stream)
-	    except yaml.YAMLError as exc:
-	        print(exc)
-	if 'winner' not in file1["info"]["outcome"]:
-		continue
+G = nx.from_numpy_matrix(playermat)
+h, a = nx.hits(G)
 
-	## Innings 1
-	fl1ind = file1["innings"][0]["1st innings"]["deliveries"]
-	# print fl1ind, len(fl1ind)
-	for i in range(len(fl1ind)):
-		bat_bowl_pair = (str(fl1ind[i][fl1ind[i].keys()[0]]["batsman"]), str(fl1ind[i][fl1ind[i].keys()[0]]["bowler"]))
-		if bat_bowl_pair not in playerlist.keys():
-			playerlist[bat_bowl_pair] = 0
-		playerlist[bat_bowl_pair] += int(fl1ind[i][fl1ind[i].keys()[0]]["runs"]["total"])
-
-	## Innings 2
-	fl1ind = file1["innings"][1]["2nd innings"]["deliveries"]
-	for i in range(len(fl1ind)):
-		bat_bowl_pair = (str(fl1ind[i][fl1ind[i].keys()[0]]["batsman"]), str(fl1ind[i][fl1ind[i].keys()[0]]["bowler"]))
-		if bat_bowl_pair not in playerlist.keys():
-			playerlist[bat_bowl_pair] = 0
-		playerlist[bat_bowl_pair] += int(fl1ind[i][fl1ind[i].keys()[0]]["runs"]["total"])
-
-with open('../data/playerdict.pkl', 'w') as fp:
-	pickle.dump(playerlist, fp)
-
-print len(playerlist)
+plt.bar(range(len(h)), h.values(), align='center')
+plt.xticks(range(len(h)), h.keys())
+plt.show(h)
