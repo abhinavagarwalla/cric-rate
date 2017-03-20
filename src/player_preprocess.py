b8802974e9cfff44fd01e4aa16c6227cc63c5f18
Abhinav Agarwalla <navabhi174@gmail.com>
2017-03-20 21:47:48 +0530

    Resolving merges

--- src/player_preprocess.py
+++ src/player_preprocess.py
@@ -109,5 +109,54 @@ def get_sorted_matches():
 	with open('../data/feed_list.pkl', 'w') as fp:
 		pickle.dump([i[0] for i in flist], fp)
 
+def get_player_teams():
+    bats = pickle.load(open('../data/batsmen.pkl'))
+    bowls = pickle.load(open('../data/bowlers.pkl'))
+    feed_list = pickle.load(open('../data/feed_list.pkl'))
+    for file in feed_list:
+        print file
+        if 'yaml' not in file:
+            continue
+        filepath = folderloc + file
+        with open(filepath, 'r') as stream:
+            try:
+                file1 = yaml.load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+        if 'winner' not in file1["info"]["outcome"]:
+            continue
+
+        teamA, teamB = file1["info"]["teams"][0], file1["info"]["teams"][1]
+        if teamA not in teams_id.keys():
+            continue
+        if teamB not in teams_id.keys():
+            continue 
+
+        # 1st Innings
+        fl1ind = file1["innings"][0]["1st innings"]["deliveries"]
+        team_bat, team_bowl = file1["innings"][0]["1st innings"]["team"], file1["innings"][1]["2nd innings"]["team"]
+        for i in range(len(fl1ind)):
+            batp = str(fl1ind[i][fl1ind[i].keys()[0]]["batsman"])
+            bowlp = str(fl1ind[i][fl1ind[i].keys()[0]]["bowler"])
+            if batp not in team_players[team_bat]:
+                team_players[team_bat].append(batp)
+            if bowlp not in team_players[team_bowl]:
+                team_players[team_bowl].append(bowlp)
+
+        #2nd Innings
+        fl1ind = file1["innings"][1]["2nd innings"]["deliveries"]
+        team_bat, team_bowl = team_bowl, team_bat
+        for i in range(len(fl1ind)):
+            batp = str(fl1ind[i][fl1ind[i].keys()[0]]["batsman"])
+            bowlp = str(fl1ind[i][fl1ind[i].keys()[0]]["bowler"])
+            if batp not in team_players[team_bat]:
+                team_players[team_bat].append(batp)
+            if bowlp not in team_players[team_bowl]:
+                team_players[team_bowl].append(bowlp)
+
+    with open('../data/player_teams.pkl','w') as fp:
+        pickle.dump(team_players, fp)
+
+# get_player_teams()
 # get_sorted_matches()
 save_dict()
