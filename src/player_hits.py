b8f62466354145781aed16063bd45dfa1e7a4b36
Abhinav Agarwalla <navabhi174@gmail.com>
2017-04-03 02:14:51 +0530

    Better Visualisation

--- src/player_hits.py
+++ src/player_hits.py
@@ -45,36 +45,87 @@ def get_hubs_auth():
 def player_graph():
     plist, bats, bowls = load_pickle()
     plist = plist[-1]
-    playermat = [[0 for i in range(len(bowls)+len(bats))] for j in range(len(bowls)+len(bats))]
+    pl = np.unique(np.append(bats, bowls))
+    msize = len(pl)
+    playermat = [[0 for i in range(msize)] for j in range(msize)]
     for key, value in plist.iteritems():
-        playermat[np.where(bowls==key[1])[0][0]][len(bowls)+np.where(bats==key[0])[0][0]] = value/50.
+        playermat[np.where(pl==key[1])[0][0]][np.where(pl==key[0])[0][0]] = value/100.
     
     G = nx.from_numpy_matrix(np.array(playermat))
     h, a = nx.hits(G)
-    h = sorted(h, key=h.get, reverse=True)[:20]
-    a = sorted(a, key=a.get, reverse=True)[:20]
+    g = G.copy()
+    for n in g.nodes():
+        if (h[n] < 0.0050) or (a[n] < 0.0050):
+            g.remove_node(n)
+#    nx.draw(g)
+    G = g.copy()
+    h, a = nx.hits(G)
+    playfin = nx.to_numpy_matrix(G)
+#    pl = 20
+#    playfin = [[0 for i in range(2*pl)] for j in range(2*pl)]
+#    for i in range(pl):
+#        for j in range(pl):
+#            print i, j, h[i], a[j]
+#            playfin[i][len(h)+j] = playermat[h[i]][a[j]]
     
-    playfin = [[0 for i in range(40)] for j in range(40)]
-    for i in range(len(h)):
-        for j in range(len(a)):
-            print i, j, h[i], a[j]
-            playfin[i][len(h)+j] = playermat[h[i]][a[j]]
-    
-    g = ig.Graph.Full(n = 40, directed = True)
-    g = g.Weighted_Adjacency(playfin, mode = "DIRECTED")
-    # g.vs["name"] = teams_id.keys()
-    g.delete_vertices(np.where(np.array(g.vs.degree())==0)[0])
-    save_graph(g)
+    gi = ig.Graph.Full(n = len(h), directed = True)
+    gi = gi.Weighted_Adjacency(playfin.tolist(), mode = "DIRECTED")
+    gi.vs["name"] = pl[h.keys()].tolist()
+    #g.delete_vertices(np.where(np.array(g.vs.degree())==0)[0])
+    save_graph(gi, pl[h.keys()].tolist(), h.values())
 
-def save_graph(g, layoutname = "fruchterman_reingold"):
+def save_graph(g, vl, cl, layoutname = "fruchterman_reingold"):
     if layoutname == "kk":
         g.write_svg("newgraph.svg", labels = "name" , layout = g.layout(layoutname))
     else:
         igraph.plot(g, 'newgraph.png', layout=g.layout(layoutname), bbox=(500, 500), margin= 50, hovermode='closest', edge_width = g.es["weight"], 
-            vertex_size=20, vertex_color = "red", vertex_label_dist = 2, vertex_label_size = 5, edge_arrow_size=1)
+            vertex_size=20, vertex_label=vl, vertex_color = 'green', vertex_label_dist = 2, vertex_label_size = 10, edge_arrow_size=0.5)
 
 
-#player_graph()
+player_graph()
 #plt.bar(range(len(h)), h.values(), align='center')
 #plt.xticks(range(len(h)), h.keys())
 #plt.show(h)
+
+
+#plist, bats, bowls = load_pickle()
+#plist = plist[-1]
+#
+##%%
+#pl = np.unique(np.append(bats, bowls))
+#msize = len(pl)
+#playermat = [[0 for i in range(msize)] for j in range(msize)]
+#for key, value in plist.iteritems():
+#    playermat[np.where(pl==key[1])[0][0]][np.where(pl==key[0])[0][0]] = value/100.
+#
+#G = nx.from_numpy_matrix(np.array(playermat))
+#h, a = nx.hits(G)
+#
+##%%
+##h = sorted(h, key=h.get, reverse=True)
+##a = sorted(a, key=a.get, reverse=True)
+#g = G.copy()
+#for n in g.nodes():
+#    if (h[n] < 0.0050) or (a[n] < 0.0050):
+#        g.remove_node(n)
+#nx.draw(g)
+#
+##%%
+#
+#G = g.copy()
+#h, a = nx.hits(G)
+#playfin = nx.to_numpy_matrix(G)
+##%%
+#pl = len(h)
+#playfin = [[0 for i in range(pl)] for j in range(pl)]
+#for i in range(pl):
+#    for j in range(pl):
+#        print i, j, h[i], a[j]
+#        playfin[i][j] = playermat[h[i]][a[j]]
+#
+##%%
+#gi = ig.Graph.Full(n = len(h), directed = True)
+#gi = gi.Weighted_Adjacency(playfin.tolist(), mode = "DIRECTED")
+#gi.vs["name"] = pl[h.keys()].tolist()
+##g.delete_vertices(np.where(np.array(g.vs.degree())==0)[0])
+#save_graph(gi, pl[h.keys()].tolist(), h.values())
